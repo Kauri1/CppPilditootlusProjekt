32f9834c839bc132cc1f816546ba768afaa1404f
Kauri <kauriremm@gmail.com>
2025-04-24 15:19:25 +0300

    QT

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <opencv2/opencv.hpp>
+#include <QApplication>
+#include <QPushButton>
+
 using namespace cv;
 
-int main() {
+int main(int argc, char *argv[]) {
     Mat image = imread("../Screenshot 2025-04-23 155822.png");
     if(image.empty()) {
         printf("Image not loaded!\n");
@@ -9,5 +12,11 @@ int main() {
     }
     imshow("Display window", image);
     waitKey(0);
-    return 0;
+
+
+    QApplication a(argc, argv);
+    QPushButton button("Hello world!", nullptr);
+    button.resize(200, 100);
+    button.show();
+    return QApplication::exec();
 }
