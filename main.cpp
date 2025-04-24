5d281b04e4aac09f7d26c786eb6bf02bce29ef3c
Kauri <kauriremm@gmail.com>
2025-04-24 15:35:40 +0300

    Display image from string

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
 #include <opencv2/opencv.hpp>
 #include <QApplication>
 #include <QPushButton>
+#include <QInputDialog>
+#include <QMessageBox>
 
 using namespace cv;
 
 int main(int argc, char *argv[]) {
-    Mat image = imread("../Screenshot 2025-04-23 155822.png");
+    QApplication app(argc, argv);
+    // Prompt the user for a string
+    bool ok;
+    QString text = QInputDialog::getText(nullptr, "Input Dialog",
+                                         "Enter your name:", QLineEdit::Normal,
+                                         "", &ok);
+
+    if (ok && !text.isEmpty()) {
+        QMessageBox::information(nullptr, "Input Received", "You entered: " + text);
+    }
+
+    //../Screenshot 2025-04-23 155822.png
+    Mat image = imread(text.toStdString());
     if(image.empty()) {
         printf("Image not loaded!\n");
-        return -1;
+    } else {
+        imshow("Display window", image);
+        waitKey(0);
     }
-    imshow("Display window", image);
-    waitKey(0);
 
-
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+    return 0;
 }
