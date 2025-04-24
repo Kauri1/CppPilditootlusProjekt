ec5ea5aad85ebbe9aebae80ca76f7cb115b20c64
Kauri <kauriremm@gmail.com>
2025-04-24 14:18:14 +0300

    Open cv?

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
 
 int main() {
+    Mat image = imread("../Screenshot 2025-04-23 155822.png");
+    if(image.empty()) {
+        printf("Image not loaded!\n");
+        return -1;
+    }
+    imshow("Display window", image);
+    waitKey(0);
     return 0;
 }
